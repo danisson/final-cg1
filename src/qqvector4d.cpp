6aa202fbdec3717b0d3f07bcc4e23769b66e23ff
99heitor <99heitor@gmail.com>
2014-09-02 11:29:25 -0300

    Mudei o parâmetro da função de multiplicação pra estrutura de dados do Daniel.

--- src/qqvector4d.cpp
+++ src/qqvector4d.cpp
@@ -2,31 +2,31 @@
 #include<cstdio>
 #include<cstring>
 #include<qstring.h>
-void QQVector4D::multiplicar(double m[4][4])
+void QQVector4D::multiplicar(TransformMatrix matrix)
 {
     //Não dá pra fazer a multiplicação direto porque ele pede um Qvector4d na multiplicação já implementada...
     double temp=0;
     int i = 0;
     for (i=0;i<4;i++){
-      temp+=m[0][i]*(this->x());
+      temp+=matrix.getMatrix(0,i)*(this->x());
     }
     this->setX(temp);
     temp=0;
     for (i=0;i<4;i++){
-     temp+=m[1][i]*(this->y());
+     temp+=matrix.getMatrix(1,i)*(this->y());
     }
     this->setY(temp);
     temp=0;
     for (i=0;i<4;i++){
-      temp+=m[2][i]*(this->z());
+      temp+=matrix.getMatrix(2,i)*(this->z());
     }
     this->setZ(temp);
-    //Na maior parte dos casos, a matriz será [0 0 0 1] nesta coluna.
+    //Na maior parte dos casos, a matrix será [0 0 0 1] nesta coluna.
     //Se precisar, podemos simplificar os calculos aqui setando o W da forma abaixo.
     //this->setW((m[3][4])*(this->w()));
     temp=0;
     for (i=0;i<4;i++){
-      temp+=m[3][i]*(this->w());
+      temp+=matrix.getMatrix(3,i)*(this->w());
     }
     this->setW(temp);
     temp=0;
