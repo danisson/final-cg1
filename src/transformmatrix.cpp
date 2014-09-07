00a9f2d8ab7cd228d8c8a5aa7937a0cf2d4e8f32
Carol <carolhmj@gmail.com>
2014-09-07 12:52:50 -0300

    supostamente as matrizes de transformacoes num estágio extremamente beta e cheio de erros :(

--- src/transformmatrix.cpp
+++ src/transformmatrix.cpp
@@ -1,7 +1,10 @@
 #include "transformmatrix.h"
 #include<cstdio>
 #include<cstring>
+#include<cmath>
+#define _USE_MATH_DEFINES
 using namespace tnw;
+using namespace std;
 
 // Métodos
 void TransformMatrix::mostrar()
@@ -125,6 +128,109 @@ double** TransformMatrix::getMatrix()
 double TransformMatrix::getMatrix(int i, int j)
 {
     return matrix[i][j];
+}
+
+double radianos(double graus){
+    return (M_PI*graus)/180;
+}
+
+TransformMatrix translacao(double a[3])
+{
+    double m[4][4] = {{1,0,0,a[0]},{0,1,0,a[1]},{0,0,1,a[2]},{0,0,0,1}};
+    return new TransformMatrix(m);
+}
+
+TransformMatrix escala(double a[3])
+{
+    double m[4][4] = {{a[0],0,0,0},{0,a[1],0,0},{0,0,a[2],0},{0,0,0,1}};
+    return new TransformMatrix(m);
+}
+
+TransformMatrix rotacaoX(double angGraus)
+{
+    angRad = radianos(angGraus);
+    double m[4][4] = {{1,0,0,0},{0,cos(angRad),-sin(angRad),0},{0,sin(angRad),cos(angRad),0},{0,0,0,1}};
+    return new TransformMatrix(m);
+}
+
+TransformMatrix rotacaoY(double angGraus)
+{
+    angRad = radianos(angGraus);
+    double m[4][4] = {{cos(angRad),0,sin(angRad),1},{0,1,0,0},{-sin(angRad),0,cos(angRad),0},{0,0,0,1}};
+    return new TransformMatrix(m);
+}
+
+TransformMatrix rotacaoZ(double angGraus)
+{
+    angRad = radianos(angGraus);
+    double m[4][4] = {{cos(angRad),-sin(angRad),0,0},{sin(angRad),cos(angRad),0,0},{0,0,1,0},{0,0,0,1}};
+    return new TransformMatrix(m);
+}
+
+TransformMatrix rotacaoVetor(double angGraus, double p1[3], double p2[3])
+{
+    //Encontra o vetor formado por esses dois pontos!
+    double x = p2[0]-p1[0];
+    double y = p2[1]-p1[1];
+    double z = p2[2]-p1[2];
+
+    //Tamanho do vetor
+    double tamanho = sqrt(x*x+y*y+z*z);
+
+    //Finalmente, transforma as coordenadas para um vetor unitário
+    double a = x/tamanho;
+    double b = y/tamanho;
+    double c = z/tamanho;
+
+    //Passo 1: Levar um dos pontos para a origem
+    //TransformMatrix MRot1 = new TransformMatrix();
+    TransformMatrix MRot1 = translacao({-p1[0],-p1[1],-p1[2]});
+
+    //Passo 2: Levar o vetor de rotação para o plano xz
+    double d = sqrt(b*b+c*c); //Comprimento da projeção do vetor no plano yz
+
+    /*double cosAngRotX = c/d;
+    double angRotX = acos(cosAngRotX);
+    angRotX = (angRotX*180)/M_PI
+    Podia definir a matriz em termos do ângulo de rotação ou direto?*/
+
+    //TransformMatrix MRot2 = new TransformMatrix();
+    double a[4][4] = {{1,0,0,0},{0,c/d,-b/d,0},{0,b/d,c/d,0},{0,0,0,1}};
+    TransformMatrix MRot2 = MRot2.setMatrix(a);
+    //Ou MRot2 = rotacaoX(angRotX);
+
+   //Passo 3: Rotacionar em relação ao eixo y para levar o vetor de rotação ao eixo z
+    TransformMatrix MRot3 = new TransformMatrix();
+    a = {{d,0,0,-a},{0,1,0,0},{a,0,d,0},{0,0,0,1}};
+    MRot3 = MRot3.setMatrix(a);
+
+    //Passo 4:Rotacionar em torno de z pelo ângulo dado
+    TransformMatrix MRot4 = new TransformMatrix();
+    MRot4 = rotacaoZ(angGraus);
+
+    //Passos 5,6,7: Inverter as transformações aplicadas!
+
+    //Passo 5: Inverter a rotação em y
+    TransformMatrix MRot5 = new TransformMatrix();
+    MRot5 = MRot5.setMatrix({ {d,0,a,0},{0,1,0,0},{-a,0,d,0},{0,0,0,1} });
+
+    //Passo 6: Inverter a rotação em x
+    TransformMatrix MRot6 = new TransformMatrix();
+    MRot6 = MRot6.setMatrix({ {1,0,0,0},{0,c/d,b/d,0},{0,-b/d,c/d,0},{0,0,0,1} });
+    //Ou MRot6 = rotacaoX(-angRotX);
+
+    //Passo 7: Inverter a translação
+    TransformMatrix MRot7 = new TransformMatrix();
+    MRot7 = translacao({p1[0],p1[1],p1[2]});
+
+    //O resultado é a multiplicação das matrizes na ordem inversa em que foram nomeadas
+
+    return MRot7*MRot6*MRot5*MRot4*MRot3*MRot2*MRot1;
+}
+
+TransformMatrix rotacaoVetorOrigem(double angGraus, double a[3])
+{
+    return rotacaoVetor(angGraus,{0,0,0},a);
 }
 
 int main()
--- src/transformmatrix.h
+++ src/transformmatrix.h
@@ -30,5 +30,20 @@ namespace tnw {
         double** getMatrix();
         double getMatrix(int i, int j);
     };
+
+    //Funções que retornam as matrizes de transformação
+
+    TransformMatrix translacao(double a[3]);
+    TransformMatrix rotacaoX(double angGraus);
+    TransformMatrix rotacaoY(double angGraus);
+    TransformMatrix rotacaoZ(double angGraus);
+    TransformMatrix rotacaoVetor(double angGraus, double p1[3], double p2[3]);
+    TransformMatrix rotacaoVetorOrigem(double angGraus, double a[3]);
+    TransformMatrix escala(double a[3]);
+
+    //Função pra transformar graus em radianos
+
+    double radianos(double graus);
+
 }
 #endif // TRANSFORMMATRIX_H
